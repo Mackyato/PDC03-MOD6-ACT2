385d935029fcf1840be6d81ac876b978407bf9a7
Jansen Cruz <jansencruz12345@gmail.com>
2024-05-24 11:07:56 +0800

    Add project files.

---
+++ Module06MVVM/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Module06MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace Module06MVVM.ViewModel
+{
+    
+    public class EmployeeViewModel
+    {
+        //public EmployeeModel EmployeeModelSet { get; set; }
+
+        //public EmployeeViewModel()
+        //{
+        //    EmployeeModelSet = new EmployeeModel
+        //    {
+        //        Id = 1,
+        //        Name = "Mark Anthony Soberano",
+        //        Email = "soberano.markanthony@auf.edu.ph",
+        //        Address = "Mabalacat City, Pampanga"
+        //    };
+        //}
+        private Services.DatabaseContext GetContext()
+        {
+            return new Services.DatabaseContext();
+        }
+
+        public int InsertEmployee(EmployeeModel obj)
+        {
+            var _dbContext = GetContext();
+            _dbContext.Employees.Add(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+
+        public async Task<List<EmployeeModel>> GetAllEmployees()
+        {
+            var _dbContext = GetContext();
+            var res = await _dbContext.Employees.ToListAsync();
+            return res;
+        }
+
+        public async Task<int> UpdateEmployee(EmployeeModel obj)
+        {
+            var _dbContext = GetContext();
+            _dbContext.Employees.Update(obj);
+            int c = await _dbContext.SaveChangesAsync();
+            return c;
+        }
+
+        public int DeleteEmployee(EmployeeModel obj)
+        {
+            var _dbContext = GetContext();
+            _dbContext.Employees.Remove(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+    }
+}
